09972d70c93389128af19907c44d44b939cb1b4a
Davey Shafik <dshafik@akamai.com>
2017-07-30 12:43:55 -0700

    Add errors

--- papi-v1/errors.go
+++ papi-v1/errors.go
@@ -1,1 +1,22 @@
 package papi
+
+import "errors"
+
+// Error constants
+const (
+	ErrInvalidPath = iota
+	ErrCriteriaNotFound
+	ErrBehaviorNotFound
+	ErrRuleNotFound
+	ErrInvalidRules
+)
+
+var (
+	ErrorMap = map[int]error{
+		ErrInvalidPath:      errors.New("Invalid Path"),
+		ErrCriteriaNotFound: errors.New("Criteria not found"),
+		ErrBehaviorNotFound: errors.New("Behavior not found"),
+		ErrRuleNotFound:     errors.New("Rule not found"),
+		ErrInvalidRules:     errors.New("Rule validation failed. See papi.Rules.Errors for details"),
+	}
+)
