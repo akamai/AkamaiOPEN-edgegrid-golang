fbe56bc0be477cb6f30fa015131a3fa8dba12473
Johanna Cherry <johanna@johannacherry.com>
2017-09-20 13:23:41 -0500

    Fix the error interface and simplify some of the names

--- configdns-v1/errors.go
+++ configdns-v1/errors.go
@@ -4,41 +4,113 @@ import (
 	"fmt"
 )
 
-type Error interface {
+type configDNSError interface {
 	error
-	IsZoneNotFound() bool
-	IsFailedToSave() bool
+	Network() bool
+	NotFound() bool
+	FailedToSave() bool
+	ValidationFailed() bool
 }
 
-type ZoneNotFoundError struct {
-	zoneName string
-	err      error
+type ZoneError struct {
+	zoneName         string
+	httpErrorMessage string
+	err              error
 }
 
-type FailedToSaveError struct {
-	err error
+func (e *ZoneError) Network() bool {
+	if httpErrorMessage != "" {
+		return true
+	}
+	return false
 }
 
-func IsZoneNotFound(err error) bool {
-	_, ok := err.(*ZoneNotFoundError)
-	return ok
+func (e *ZoneError) NotFound() bool {
+	if e.err == nil && httpErrorMessage == "" {
+		return true
+	}
+	return false
 }
 
-func IsFailedToSave(err error) bool {
-	_, ok := err.(*FailedToSaveError)
-	return ok
+func (e *ZoneError) FailedToSave() bool {
+	return false
 }
 
-func (e *ZoneNotFoundError) Error() string {
+func (e *ZoneError) ValidationFailed() bool {
+	return false
+}
+
+func (e *ZoneError) Error() string {
 	if e == nil {
 		return "<nil>"
 	}
-	return fmt.Sprintf("Zone \"%s\" not found, creating new zone.", e.zoneName)
+
+	if e.Network() {
+		return fmt.Sprintf("Zone \"%s\" network error: [%s]", e.zoneName, e.httpErrorMessage)
+	}
+
+	if e.NotFound() {
+		return fmt.Sprintf("Zone \"%s\" not found.", e.zoneName)
+	}
+
+	if e.FailedToSave() {
+		return fmt.Sprintf("Zone \"%s\" failed to save: [%s]", e.zoneName, e.err.Error())
+	}
+
+	if e.ValidationFailed() {
+		return fmt.Sprintf("Zone \"%s\" validation failed: [%s]", e.zoneName, e.err.Error())
+	}
 }
 
-func (e *FailedToSaveError) Error() string {
+type RecordError struct {
+	fieldName        string
+	httpErrorMessage string
+	err              error
+}
+
+func (e *ZoneError) Network() bool {
+	if httpErrorMessage != "" {
+		return true
+	}
+	return false
+}
+
+func (e *RecordError) NotFound() bool {
+	return false
+}
+
+func (e *RecordError) FailedToSave() bool {
+	if fieldName == "" {
+		return true
+	}
+	return false
+}
+
+func (e *RecordError) ValidationFailed() bool {
+	if fieldName != "" {
+		return true
+	}
+	return false
+}
+
+func (e *RecordError) Error() string {
 	if e == nil {
 		return "<nil>"
 	}
-	return fmt.Sprintf("Unable to save record (%s)", e.err.Error())
+
+	if e.Network() {
+		return fmt.Sprintf("Record network error: [%s]", e.httpErrorMessage)
+	}
+
+	if e.NotFound() {
+		return fmt.Sprintf("Record not found.")
+	}
+
+	if e.FailedToSave() {
+		return fmt.Sprintf("Record failed to save: [%s]", e.err.Error())
+	}
+
+	if e.ValidationFailed() {
+		return fmt.Sprintf("Record validation failed for field [%s]: [%s]", e.fieldName, e.err.Error())
+	}
 }
