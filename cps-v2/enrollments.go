60b7e5b10a55179ebc43380fab576717c67bf59d
Ted Elwartowski <elwartowski@unfpa.org>
2018-03-14 08:20:35 -0500

    Support for CPS enrollments - includes types and functions for getting available enrollments using the CPS v2 API

---
+++ cps-v2/enrollments.go
@@ -0,0 +1,123 @@
+package cps
+
+import (
+  "github.com/akamai/AkamaiOPEN-edgegrid-golang/edgegrid"
+  "github.com/akamai/AkamaiOPEN-edgegrid-golang/client-v1"
+)
+
+// Enrollments represents a CPS enrollments list
+type Enrollments struct {
+  Enrollments []Enrollment `json:"enrollments"`
+}
+
+// NewEnrollments creates an empty Enrollments
+func NewEnrollments() *Enrollments {
+  e := &Enrollments{}
+  return e
+}
+
+// Get populates Enrollments with the list of avaiable enrollments
+//
+// API Docs: https://developer.akamai.com/api/luna/cps/resources.html#getenrollments
+func (e *Enrollments) Get(config edgegrid.Config) error {
+  if req, err := client.NewRequest(
+    config,
+    "GET",
+    "/cps/v2/enrollments",
+    nil,
+    ); err == nil {
+    req.Header.Add("Accept", "application/vnd.akamai.cps.enrollments.v4+json")
+    if res, err := client.Do(config, req); err == nil {
+      if client.IsError(res) {
+    		return client.NewAPIError(res)
+    	}
+      if err := client.BodyJSON(res, e); err != nil {
+    		return err
+    	}
+    } else {
+      return err
+    }
+  } else {
+    return err
+  }
+
+  return nil
+}
+
+// Enrollment represents a CPS enrollment resource
+type Enrollment struct {
+  Location string `json:"location"`
+  RootAuth string `json:"ra"`
+  ValidationType string `json:"validationType"`
+  CertificateType string `json:"certificateType"`
+  NetworkConfiguration NetworkConfiguration `json:"networkConfiguration"`
+  CSR CSR `json:"csr"`
+  SignatureAlgorithm string `json:"signatureAlgorithm"`
+  ChangeManagement bool `json:"changeManagement"`
+  Org Org `json:"org"`
+  AdminContact Contact `json:"adminContact"`
+  TechContact Contact `json:"techContact"`
+  ThirdParty ThirdParty `json:"thirdParty"`
+  EnableMultiStackedCertificates bool `json:"enableMultiStackedCertificates"`
+  PendingChanges []string `json:"pendingChanges"`
+}
+
+// ThirdParty represents an enrollment thirdParty
+type ThirdParty struct {
+  ExcludeSans bool `json:"excludeSans"`
+}
+
+// Org represents an enrollment org
+type Org struct {
+  Name string `json:"name"`
+  AddressLineOne string `json:"addressLineOne"`
+  AddressLineTwo string `json:"addressLineTwo"`
+  City string `json:"city"`
+  Region string `json:"region"`
+  PostalCode string `json:"postalCode"`
+  Country string `json:"country"`
+  Phone string `json:"phone"`
+}
+
+// Contact represents an enrollment contact (adminContact|techContact)
+type Contact struct {
+  FirstName string `json:"firstName"`
+  LastName string `json:"lastName"`
+  Phone string `json:"phone"`
+  Email string `json:"email"`
+  AddressLineOne string `json:"addressLineOne"`
+  AddressLineTwo string `json:"addressLineTwo"`
+  City string `json:"city"`
+  Country string `json:"country"`
+  OrganizationName string `json:"organizationName"`
+  PostalCode string `json:"postalCode"`
+  Region string `json:"region"`
+  Title string `json:"title"`
+}
+
+// NetworkConfiguration represents an enrollment networkConfiguration
+type NetworkConfiguration struct {
+  Geography string `json:"geography"`
+  SecureNetwork string `json:"secureNetwork"`
+  MustHaveCiphers string `json:"mustHaveCiphers"`
+  PreferredCiphers string `json:"preferredCiphers"`
+  DisallowedTlsVersions []string `json:"disallowedTlsVersions"`
+  SNI SNI `json:"sni"`
+}
+
+// SNI represents a networkConfiguration sni
+type SNI struct {
+  CloneDnsNames bool `json:"cloneDnsNames"`
+  DnsNames []string `json:"dnsNames"`
+}
+
+// CSR represents an enrollment csr
+type CSR struct {
+  CommonName string `json:"cn"`
+  Country string `json:"c"`
+  State string `json:"st"`
+  Location string `json:"l"`
+  Org string `json:"o"`
+  OrgUnit string `json:"ou"`
+  SANS []string `json:"sans"`
+}
