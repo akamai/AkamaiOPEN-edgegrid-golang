449f885e738b206a57c11cf441b84f398e4a2c36
Ted Elwartowski <elwartowski@unfpa.org>
2018-03-15 19:15:22 -0500

    migrated to config.go

--- cps-v2/v2.go
+++
@@ -1,9 +0,0 @@
-package cps
-
-const (
-  // Value for the Accept Header in API requests
-  ACCEPT_HEADER = "application/vnd.akamai.cps.enrollments.v4+json"
-
-  // API Resource URI Constants
-  GET_ENROLLMENTS_URI = "/cps/v2/enrollments"
-)
