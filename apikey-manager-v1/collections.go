b093154b787b9c190ec89d7c7b9ee11c9edebad4
Johanna Cherry <johanna@johannacherry.com>
2018-10-07 22:52:11 -0500

    Add get collection multi to look up collection IDs by name/regex

--- apikey-manager-v1/collections.go
+++ apikey-manager-v1/collections.go
@@ -1,7 +1,10 @@
 package apikeymanager
 
 import (
+	"errors"
 	"fmt"
+	"regexp"
+	"strconv"
 
 	"github.com/akamai/AkamaiOPEN-edgegrid-golang/client-v1"
 )
@@ -116,6 +119,66 @@ func GetCollection(collectionId int) (*Collection, error) {
 	}
 
 	return rep, nil
+}
+
+func GetCollectionMulti(collection string) (*Collections, error) {
+	id, err := strconv.Atoi(collection)
+	if err == nil {
+		req, err := client.NewJSONRequest(
+			Config,
+			"GET",
+			fmt.Sprintf("/apikey-manager-api/v1/collections/%d", id),
+			nil,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		res, err := client.Do(Config, req)
+
+		if err != nil {
+			return nil, err
+		}
+
+		if client.IsError(res) {
+			return nil, client.NewAPIError(res)
+		}
+
+		rep := &Collection{}
+		if err = client.BodyJSON(res, rep); err != nil {
+			return nil, err
+		}
+
+		return &Collections{*rep}, nil
+	} else {
+		collections, err := ListCollections()
+		if err != nil {
+			return nil, err
+		}
+
+		if collection[len(collection):] == "*" {
+			collection = collection[:len(collection)-1] + ".*"
+		}
+
+		ret := Collections{}
+		for _, r := range *collections {
+			matched, err := regexp.MatchString(collection, r.Name)
+			if err != nil {
+				return nil, err
+			}
+
+			if matched {
+				ret = append(ret, r)
+			}
+		}
+
+		if len(ret) > 0 {
+			return &ret, nil
+		}
+
+		return nil, errors.New("Collection not found.")
+	}
 }
 
 func CollectionAclAllow(collectionId int, acl []string) (*Collection, error) {
