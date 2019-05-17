aef2948f51b5beebf69915484f8a8174ddf12a6c
martinstibbe <martin@akava.io>
2019-05-16 20:14:35 -0500

    Travis test changes

--- cps-v2/certificate_settings_test.go
+++ cps-v2/certificate_settings_test.go
@@ -3,11 +3,11 @@ package cps
 import (
 	"testing"
 
-	cps "github.com/akamai/AkamaiOPEN-edgegrid-golang/cps-v2"
+	//cps "github.com/akamai/AkamaiOPEN-edgegrid-golang/cps-v2"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestSHAConversion(t *testing.T) {
 	assert.Equal(t, "dosty", "everts")
-	assert.Equal(t, cps.SHA256, cps.SHA("SHA-256"))
+	assert.Equal(t, SHA256, SHA("SHA-256"))
 }
