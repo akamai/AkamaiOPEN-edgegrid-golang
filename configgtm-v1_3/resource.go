9a6b4b3f0858f60c13a58f4ffa6a32d104a1fd27
Ed Lynes <elynes@akamai.com>
2019-07-22 11:33:57 -0400

    PR review changes

--- configgtm-v1_3/resource.go
+++ configgtm-v1_3/resource.go
@@ -3,7 +3,7 @@ package configgtm
 import (
 	"github.com/edglynes/AkamaiOPEN-edgegrid-golang/client-v1"
 
-        "fmt"
+	"fmt"
 )
 
 //
@@ -11,113 +11,115 @@ import (
 // Based on 1.3 schema
 //
 
+// ResourceInstance
 type ResourceInstance struct {
-        DatacenterId             int               `json:"datacenterId"`
-        UseDefaultLoadObject     bool              `json:"seDefaultLoadObject,omitempty"`
-        LoadObject
+	DatacenterId         int  `json:"datacenterId"`
+	UseDefaultLoadObject bool `json:"seDefaultLoadObject,omitempty"`
+	LoadObject
 }
 
-// Resource represents a GTM resource   
+// Resource represents a GTM resource
 type Resource struct {
-        Type                              string                 `json:"type"`
-        HostHeader                        string                 `json:"hostHeader,omitempty"`
-        LeastSquaresDecay                 int                    `json:"leastSquaresDecay,omitempty"`
-        Description                       string                 `json:"description,omitempty"`
-        LeaderString                      string                 `json:"leaderString,omitempty"`
-        ConstrainedProperty               string                 `json:"constrainedProperty,omitempty"`
-        ResourceInstances                 []*ResourceInstance    `json:"resourceInstances,omitempty"`
-        AggregationType                   string                 `json:"aggregationType"`
-        Links                             []*Link                `json:"links,omitempty"`
-        LoadImbalancePercent              float64                `json:"loadImbalancePercent,omitempty"`
-        UpperBound                        int                    `json:"upperBound,omitempty"`
-        Name                              string                 `json:"name"`
-        MaxUMultiplicativeIncrement       float64                `json:"maxUMultiplicativeIncrement,omitempty"`
-        DecayRate                         float64                `json:"decayRate,omitempty"`
+	Type                        string              `json:"type"`
+	HostHeader                  string              `json:"hostHeader,omitempty"`
+	LeastSquaresDecay           int                 `json:"leastSquaresDecay,omitempty"`
+	Description                 string              `json:"description,omitempty"`
+	LeaderString                string              `json:"leaderString,omitempty"`
+	ConstrainedProperty         string              `json:"constrainedProperty,omitempty"`
+	ResourceInstances           []*ResourceInstance `json:"resourceInstances,omitempty"`
+	AggregationType             string              `json:"aggregationType"`
+	Links                       []*Link             `json:"links,omitempty"`
+	LoadImbalancePercent        float64             `json:"loadImbalancePercent,omitempty"`
+	UpperBound                  int                 `json:"upperBound,omitempty"`
+	Name                        string              `json:"name"`
+	MaxUMultiplicativeIncrement float64             `json:"maxUMultiplicativeIncrement,omitempty"`
+	DecayRate                   float64             `json:"decayRate,omitempty"`
 }
 
+// ResourceList is the structure returned by List Resources
 type ResourceList struct {
-        ResourceItems            []*Resource       `json:"items"`
+	ResourceItems []*Resource `json:"items"`
 }
 
-// NewResourceInstance instantiates a new ResourceInstance. 
-func (rsrc *Resource) NewResourceInstance(dcid int) *ResourceInstance {
- 
-        return &ResourceInstance{DatacenterId: dcid}  
+// NewResourceInstance instantiates a new ResourceInstance.
+func (rsrc *Resource) NewResourceInstance(dcID int) *ResourceInstance {
+
+	return &ResourceInstance{DatacenterId: dcID}
 
 }
 
 // NewResource creates a new Resource object.
-func NewResource(resourcename string) *Resource {
-	resource := &Resource{Name: resourcename}
-        return resource
+func NewResource(name string) *Resource {
+	resource := &Resource{Name: name}
+	return resource
 }
 
 // ListResources retreieves all Resources in the specified domain.
-func ListResources(domainname string) ([]*Resource, error) {
-        rsrcs := &ResourceList{}
-        req, err := client.NewRequest(
-                Config,
-                "GET",
-                "/config-gtm/v1/domains/"+domainname+"/resources",
-                nil,
-        )
-        if err != nil {
-                return nil, err
-        }       
-
-        SetHeader(req)
-
-        printHttpRequest(req, true)
-
-        res, err := client.Do(Config, req)
-        if err != nil {
-                return nil, err
-        }
-
-        printHttpResponse(res, true)
-
-        if client.IsError(res) && res.StatusCode != 404 {
-                return nil, client.NewAPIError(res)
-        } else if res.StatusCode == 404 {
-                return nil, &CommonError{entityName: "Resources"}
-        } 
-        err = client.BodyJSON(res, rsrcs)
-        if err != nil {
-                return nil, err
-        }
-
-        return rsrcs.ResourceItems, nil
-         
-}
-
-// GetResource retrieves a Resource with the given name in the specified domain.
-func GetResource(resourcename, domainname string) (*Resource, error) {
-	rsc := NewResource(resourcename)
+func ListResources(domainName string) ([]*Resource, error) {
+	rsrcs := &ResourceList{}
 	req, err := client.NewRequest(
 		Config,
 		"GET",
-		"/config-gtm/v1/domains/"+domainname+"/resources/"+resourcename,
+		fmt.Sprintf("/config-gtm/v1/domains/%s/resources", domainName),
 		nil,
 	)
 	if err != nil {
 		return nil, err
 	}
 
-        SetHeader(req)
+	setVersionHeader(req, schemaVersion)
 
-        printHttpRequest(req, true)
+	printHttpRequest(req, true)
 
 	res, err := client.Do(Config, req)
 	if err != nil {
 		return nil, err
 	}
 
-        printHttpResponse(res, true)
+	printHttpResponse(res, true)
 
 	if client.IsError(res) && res.StatusCode != 404 {
 		return nil, client.NewAPIError(res)
 	} else if res.StatusCode == 404 {
-		return nil, &CommonError{entityName: "Resource", name: resourcename}
+		return nil, &CommonError{entityName: "Resources"}
+	}
+	err = client.BodyJSON(res, rsrcs)
+	if err != nil {
+		return nil, err
+	}
+
+	return rsrcs.ResourceItems, nil
+
+}
+
+// GetResource retrieves a Resource with the given name in the specified domain.
+func GetResource(name, domainName string) (*Resource, error) {
+	rsc := NewResource(name)
+	req, err := client.NewRequest(
+		Config,
+		"GET",
+		fmt.Sprintf("/config-gtm/v1/domains/%s/resources/%s", domainName, name),
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	setVersionHeader(req, schemaVersion)
+
+	printHttpRequest(req, true)
+
+	res, err := client.Do(Config, req)
+	if err != nil {
+		return nil, err
+	}
+
+	printHttpResponse(res, true)
+
+	if client.IsError(res) && res.StatusCode != 404 {
+		return nil, client.NewAPIError(res)
+	} else if res.StatusCode == 404 {
+		return nil, &CommonError{entityName: "Resource", name: name}
 	} else {
 		err = client.BodyJSON(res, rsc)
 		if err != nil {
@@ -128,130 +130,125 @@ func GetResource(resourcename, domainname string) (*Resource, error) {
 	}
 }
 
+// Create the resource identified by the receiver argument in the specified domain.
+func (rsrc *Resource) Create(domainName string) (*ResourceResponse, error) {
 
-// Create the resource identified by the receiver argument in the specified domain.                        
-func (rsrc *Resource) Create(domainname string) (*ResourceResponse, error) {
+	// Use common code. Any specific validation needed?
 
-        // Use common code. Any specific validation needed?
-
-        return rsrc.save(domainname)
+	return rsrc.save(domainName)
 
 }
 
 // Update the resourceidentified in the receiver argument in the specified domain.
-func (rsrc *Resource) Update(domainname string) (*ResponseStatus, error) {
+func (rsrc *Resource) Update(domainName string) (*ResponseStatus, error) {
 
-        // common code
-   
-        stat, err := rsrc.save(domainname)
-        if err != nil {
-                return nil, err
-        }
-        return stat.Status, err
+	// common code
+
+	stat, err := rsrc.save(domainName)
+	if err != nil {
+		return nil, err
+	}
+	return stat.Status, err
 
 }
 
 // Save Resource in given domain. Common path for Create and Update.
-func (rsrc *Resource) save(domainname string) (*ResourceResponse, error) {
+func (rsrc *Resource) save(domainName string) (*ResourceResponse, error) {
 
-        fmt.Println("Creating request!!!!")
-        req, err := client.NewJSONRequest(
-                Config,
-                "PUT",
-                "/config-gtm/v1/domains/"+domainname+"/resources/"+rsrc.Name,
-                rsrc,
-        )
-        fmt.Println("BACK!!!!!!!!!!!!!!!!")
-      
-        if err != nil {
-                return nil, err
-        }
+	fmt.Println("Creating request!!!!")
+	req, err := client.NewJSONRequest(
+		Config,
+		"PUT",
+		fmt.Sprintf("/config-gtm/v1/domains/%s/resources/%s", domainName, rsrc.Name),
+		rsrc,
+	)
 
-        fmt.Println("Setting header!!!")
+	if err != nil {
+		return nil, err
+	}
 
-        SetHeader(req)
+	setVersionHeader(req, schemaVersion)
 
-        printHttpRequest(req, true)
+	printHttpRequest(req, true)
 
-        res, err := client.Do(Config, req)
+	res, err := client.Do(Config, req)
 
-        printHttpResponse(res, true)
+	printHttpResponse(res, true)
 
-        // Network error
-        if err != nil {
-                return nil, &CommonError{
-                        entityName:       "Resource",
-                        name:             rsrc.Name,
-                        httpErrorMessage: err.Error(),
-                        err:              err,
-                }
-        }
+	// Network error
+	if err != nil {
+		return nil, &CommonError{
+			entityName:       "Resource",
+			name:             rsrc.Name,
+			httpErrorMessage: err.Error(),
+			err:              err,
+		}
+	}
 
-        // API error
-        if client.IsError(res) {
-                err := client.NewAPIError(res)
-                return nil, &CommonError{entityName: "Resource", name: rsrc.Name, apiErrorMessage: err.Detail, err: err}
-        }
+	// API error
+	if client.IsError(res) {
+		err := client.NewAPIError(res)
+		return nil, &CommonError{entityName: "Resource", name: rsrc.Name, apiErrorMessage: err.Detail, err: err}
+	}
 
-        responseBody := &ResourceResponse{}
-        // Unmarshall whole response body for updated entity and in case want status
-        err = client.BodyJSON(res, responseBody)
-        if err != nil {
-                return nil, err
-        }
+	responseBody := &ResourceResponse{}
+	// Unmarshall whole response body for updated entity and in case want status
+	err = client.BodyJSON(res, responseBody)
+	if err != nil {
+		return nil, err
+	}
 
-        return responseBody, nil
+	return responseBody, nil
 
 }
 
-// Delete the resource identified in the receiver argument from the specified domain. 
-func (rsrc *Resource) Delete(domainname string) (*ResponseStatus, error) {
+// Delete the resource identified in the receiver argument from the specified domain.
+func (rsrc *Resource) Delete(domainName string) (*ResponseStatus, error) {
 
-        req, err := client.NewRequest(
-                Config,
-                "DELETE",
-                "/config-gtm/v1/domains/"+domainname+"/resources/"+rsrc.Name,
-                nil,
-        )
-        if err != nil {
-                return nil, err
-        }
+	req, err := client.NewRequest(
+		Config,
+		"DELETE",
+		fmt.Sprintf("/config-gtm/v1/domains/%s/resources/%s", domainName, rsrc.Name),
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
 
-        SetHeader(req)
+	setVersionHeader(req, schemaVersion)
 
-        printHttpRequest(req, true)
+	printHttpRequest(req, true)
 
-        res, err := client.Do(Config, req)
-        if err != nil {
-                return nil, err
-        }
+	res, err := client.Do(Config, req)
+	if err != nil {
+		return nil, err
+	}
 
-        printHttpResponse(res, true)
+	printHttpResponse(res, true)
 
-        // Network error
-        if err != nil {
-                return nil, &CommonError{
-                        entityName:       "Resource",
-                        name:             rsrc.Name,
-                        httpErrorMessage: err.Error(),
-                        err:              err,
-                }
-        }
+	// Network error
+	if err != nil {
+		return nil, &CommonError{
+			entityName:       "Resource",
+			name:             rsrc.Name,
+			httpErrorMessage: err.Error(),
+			err:              err,
+		}
+	}
 
-        // API error
-        if client.IsError(res) {
-                err := client.NewAPIError(res)
-                return nil, &CommonError{entityName: "Resource", name:rsrc.Name, apiErrorMessage: err.Detail, err: err}
-        }
+	// API error
+	if client.IsError(res) {
+		err := client.NewAPIError(res)
+		return nil, &CommonError{entityName: "Resource", name: rsrc.Name, apiErrorMessage: err.Detail, err: err}
+	}
 
-        responseBody := &ResponseBody{}
-        // Unmarshall whole response body in case want status
-        err = client.BodyJSON(res, responseBody)
-        if err != nil {
-                return nil, err
-        }
+	responseBody := &ResponseBody{}
+	// Unmarshall whole response body in case want status
+	err = client.BodyJSON(res, responseBody)
+	if err != nil {
+		return nil, err
+	}
 
-        return responseBody.Status, nil
+	return responseBody.Status, nil
 
 }
-
--- configgtm-v1_3/resource_test.go
+++ configgtm-v1_3/resource_test.go
@@ -3,21 +3,20 @@ package configgtm
 import (
 	"testing"
 
-        "github.com/akamai/AkamaiOPEN-edgegrid-golang/jsonhooks-v1"
+	"github.com/akamai/AkamaiOPEN-edgegrid-golang/jsonhooks-v1"
 
 	"github.com/h2non/gock"
 	"github.com/stretchr/testify/assert"
 
-        "reflect"
-        "fmt"
+	"fmt"
 )
 
 var GtmTestResource = "testResource"
 
 func instantiateResource() *Resource {
 
-        resource := NewResource("dummy") 
-        resourceData := []byte(`{
+	resource := NewResource("dummy")
+	resourceData := []byte(`{
                                 "aggregationType" : "median",
                                 "constrainedProperty" : null,
                                 "decayRate" : null,
@@ -42,24 +41,24 @@ func instantiateResource() *Resource {
                                         "href" : "https://akaa-32qkzqewderdchot-d3uwbyqc4pqi2c5l.luna-dev.akamaiapis.net/config-gtm/v1/domains/gtmdomtest.akadns.net/resources/testResource"
                                 } ]
                        }`)
-        jsonhooks.Unmarshal(resourceData, resource)
+	jsonhooks.Unmarshal(resourceData, resource)
 
-        return resource
+	return resource
 
 }
 
 // Verify ListResources. Should pass, e.g. no API errors and non nil list.
 func TestListResources(t *testing.T) {
 
-        defer gock.Off()
+	defer gock.Off()
 
-        mock := gock.New("https://akaa-baseurl-xxxxxxxxxxx-xxxxxxxxxxxxx.luna.akamaiapis.net/config-gtm/v1/domains/"+gtmTestDomain+"/resources")
-        mock.
-                Get("/config-gtm/v1/domains/"+gtmTestDomain+"/resources").
-                HeaderPresent("Authorization").
-                Reply(200).
-                SetHeader("Content-Type", "application/vnd.config-gtm.v1.3+json;charset=UTF-8").
-                BodyString(`{
+	mock := gock.New("https://akaa-baseurl-xxxxxxxxxxx-xxxxxxxxxxxxx.luna.akamaiapis.net/config-gtm/v1/domains/" + gtmTestDomain + "/resources")
+	mock.
+		Get("/config-gtm/v1/domains/"+gtmTestDomain+"/resources").
+		HeaderPresent("Authorization").
+		Reply(200).
+		SetHeader("Content-Type", "application/vnd.config-gtm.v1.3+json;charset=UTF-8").
+		BodyString(`{
                         "items" : [ {
                                 "aggregationType" : "median",
                                 "constrainedProperty" : null,
@@ -81,31 +80,31 @@ func TestListResources(t *testing.T) {
                         } ]
                }`)
 
-       Init(config)
-       resourceList, err := ListResources(gtmTestDomain)
-       assert.NoError(t, err)
-       assert.NotEqual(t, resourceList, nil)
+	Init(config)
+	resourceList, err := ListResources(gtmTestDomain)
+	assert.NoError(t, err)
+	assert.NotEqual(t, resourceList, nil)
 
-       if len(resourceList) > 0 {
-               firstResource := resourceList[0]
-               assert.Equal(t, firstResource.Name, GtmTestResource)
-       } else {
-               t.Fatal("List empty!")
-       }
+	if len(resourceList) > 0 {
+		firstResource := resourceList[0]
+		assert.Equal(t, firstResource.Name, GtmTestResource)
+	} else {
+		t.Fatal("List empty!")
+	}
 }
 
 // Depends on CreateResource
 func TestGetResource(t *testing.T) {
 
-        defer gock.Off()
+	defer gock.Off()
 
-        mock := gock.New("https://akaa-baseurl-xxxxxxxxxxx-xxxxxxxxxxxxx.luna.akamaiapis.net/config-gtm/v1/domains/"+gtmTestDomain+"/resources/"+GtmTestResource)
-        mock.
-                Get("/config-gtm/v1/domains/"+gtmTestDomain+"/resources/"+GtmTestResource).
-                HeaderPresent("Authorization").
-                Reply(200).
-                SetHeader("Content-Type", "application/vnd.config-gtm.v1.3+json;charset=UTF-8").
-                BodyString(`{
+	mock := gock.New("https://akaa-baseurl-xxxxxxxxxxx-xxxxxxxxxxxxx.luna.akamaiapis.net/config-gtm/v1/domains/" + gtmTestDomain + "/resources/" + GtmTestResource)
+	mock.
+		Get("/config-gtm/v1/domains/"+gtmTestDomain+"/resources/"+GtmTestResource).
+		HeaderPresent("Authorization").
+		Reply(200).
+		SetHeader("Content-Type", "application/vnd.config-gtm.v1.3+json;charset=UTF-8").
+		BodyString(`{
                         "aggregationType" : "median",
                         "constrainedProperty" : null,
                         "decayRate" : null,
@@ -125,50 +124,48 @@ func TestGetResource(t *testing.T) {
                         } ]
               }`)
 
-       Init(config)
+	Init(config)
 
-       testResource, err := GetResource(GtmTestResource, gtmTestDomain)
-       assert.NoError(t, err)
-       assert.IsType(t, &Resource{}, testResource)
-       assert.Equal(t, GtmTestResource, testResource.Name)
+	testResource, err := GetResource(GtmTestResource, gtmTestDomain)
+	assert.NoError(t, err)
+	assert.IsType(t, &Resource{}, testResource)
+	assert.Equal(t, GtmTestResource, testResource.Name)
 
 }
 
 // Verify failed case for GetResource. Should pass, e.g. no API errors and domain not found
 func TestGetBadResource(t *testing.T) {
 
-        defer gock.Off()
+	defer gock.Off()
 
-        mock := gock.New("https://akaa-baseurl-xxxxxxxxxxx-xxxxxxxxxxxxx.luna.akamaiapis.net/config-gtm/v1/domains/"+gtmTestDomain+"/resources/somebadname")
-        mock.
-                Get("/config-gtm/v1/domains/"+gtmTestDomain+"/resources/somebadname").
-                HeaderPresent("Authorization").
-                Reply(404).
-                SetHeader("Content-Type", "application/vnd.config-gtm.v1.3+json;charset=UTF-8").
-                BodyString(`{
+	mock := gock.New("https://akaa-baseurl-xxxxxxxxxxx-xxxxxxxxxxxxx.luna.akamaiapis.net/config-gtm/v1/domains/" + gtmTestDomain + "/resources/somebadname")
+	mock.
+		Get("/config-gtm/v1/domains/"+gtmTestDomain+"/resources/somebadname").
+		HeaderPresent("Authorization").
+		Reply(404).
+		SetHeader("Content-Type", "application/vnd.config-gtm.v1.3+json;charset=UTF-8").
+		BodyString(`{
                         }`)
 
-       Init(config)
+	Init(config)
 
-       _, err := GetResource("somebadname", gtmTestDomain)
-       assert.Error(t, err)
-       // Shouldn't have found
-       // Only CommonErrror thrown is Not 
-       assert.Equal(t, reflect.TypeOf(err).String(), "*CommonError")
+	_, err := GetResource("somebadname", gtmTestDomain)
+        // Shouldn't have found
+	assert.Error(t, err)
 
 }
 
-// Test Create resource. 
+// Test Create resource.
 func TestCreateResource(t *testing.T) {
 
-        defer gock.Off()
-        mock := gock.New("https://akaa-baseurl-xxxxxxxxxxx-xxxxxxxxxxxxx.luna.akamaiapis.net/config-gtm/v1/domains/"+gtmTestDomain+"/resources/"+GtmTestResource)
-        mock.
-                Put("/config-gtm/v1/domains/"+gtmTestDomain+"/resources/"+GtmTestResource).
-                HeaderPresent("Authorization").
-                Reply(200).
-                SetHeader("Content-Type", "application/vnd.config-gtm.v1.3+json;charset=UTF-8").
-                BodyString(`{
+	defer gock.Off()
+	mock := gock.New("https://akaa-baseurl-xxxxxxxxxxx-xxxxxxxxxxxxx.luna.akamaiapis.net/config-gtm/v1/domains/" + gtmTestDomain + "/resources/" + GtmTestResource)
+	mock.
+		Put("/config-gtm/v1/domains/"+gtmTestDomain+"/resources/"+GtmTestResource).
+		HeaderPresent("Authorization").
+		Reply(200).
+		SetHeader("Content-Type", "application/vnd.config-gtm.v1.3+json;charset=UTF-8").
+		BodyString(`{
                         "resource" : {
                                 "aggregationType" : "median",
                                 "constrainedProperty" : null,
@@ -207,41 +204,41 @@ func TestCreateResource(t *testing.T) {
                         }
                 }`)
 
-       Init(config)
+	Init(config)
 
-       testResource := NewResource(GtmTestResource)
-       testResource.AggregationType = "median"
-       testResource.Type = "Download score"
+	testResource := NewResource(GtmTestResource)
+	testResource.AggregationType = "median"
+	testResource.Type = "Download score"
 
-       // Create a Resource Instance
-       var instanceSlice []*ResourceInstance
-       instance := testResource.NewResourceInstance(3131)
-       instanceSlice = append(instanceSlice, instance) 
-       testResource.ResourceInstances = instanceSlice
-        
-       // do the create
+	// Create a Resource Instance
+	var instanceSlice []*ResourceInstance
+	instance := testResource.NewResourceInstance(3131)
+	instanceSlice = append(instanceSlice, instance)
+	testResource.ResourceInstances = instanceSlice
 
-       fmt.Println("Calling Create!!!")
-       statresp, err := testResource.Create(gtmTestDomain)
-       fmt.Println("Returned")
-       assert.NoError(t, err)
-      
-       assert.IsType(t, &Resource{}, statresp.Resource)
-       assert.Equal(t, GtmTestResource, statresp.Resource.Name)
+	// do the create
+
+	fmt.Println("Calling Create!!!")
+	statresp, err := testResource.Create(gtmTestDomain)
+	fmt.Println("Returned")
+	assert.NoError(t, err)
+
+	assert.IsType(t, &Resource{}, statresp.Resource)
+	assert.Equal(t, GtmTestResource, statresp.Resource.Name)
 
 }
 
 func TestUpdateResource(t *testing.T) {
 
-        defer gock.Off()
+	defer gock.Off()
 
-        mock := gock.New("https://akaa-baseurl-xxxxxxxxxxx-xxxxxxxxxxxxx.luna.akamaiapis.net/config-gtm/v1/domains/"+gtmTestDomain+"/resources/"+GtmTestResource)
-        mock.
-                Put("/config-gtm/v1/domains/"+gtmTestDomain+"/resources/"+GtmTestResource).
-                HeaderPresent("Authorization").
-                Reply(200).
-                SetHeader("Content-Type", "application/vnd.config-gtm.v1.3+json;charset=UTF-8").
-                BodyString(`{
+	mock := gock.New("https://akaa-baseurl-xxxxxxxxxxx-xxxxxxxxxxxxx.luna.akamaiapis.net/config-gtm/v1/domains/" + gtmTestDomain + "/resources/" + GtmTestResource)
+	mock.
+		Put("/config-gtm/v1/domains/"+gtmTestDomain+"/resources/"+GtmTestResource).
+		HeaderPresent("Authorization").
+		Reply(200).
+		SetHeader("Content-Type", "application/vnd.config-gtm.v1.3+json;charset=UTF-8").
+		BodyString(`{
                         "resource" : {
                                 "aggregationType" : "median",
                                 "constrainedProperty" : null,
@@ -280,30 +277,30 @@ func TestUpdateResource(t *testing.T) {
                         }
                 }`)
 
-       Init(config)
+	Init(config)
 
-       testResource := instantiateResource()
+	testResource := instantiateResource()
 
-       newDecay := 0.5
-       testResource.DecayRate = newDecay
-       stat, err := testResource.Update(gtmTestDomain)
-       assert.NoError(t, err)
+	newDecay := 0.5
+	testResource.DecayRate = newDecay
+	stat, err := testResource.Update(gtmTestDomain)
+	assert.NoError(t, err)
 
-       assert.Equal(t, stat.ChangeId, "5bb9f131-99c8-43ff-afd2-a6ce34db8b95")
+	assert.Equal(t, stat.ChangeId, "5bb9f131-99c8-43ff-afd2-a6ce34db8b95")
 
 }
 
 func TestDeleteResource(t *testing.T) {
 
-        defer gock.Off()
+	defer gock.Off()
 
-        mock := gock.New("https://akaa-baseurl-xxxxxxxxxxx-xxxxxxxxxxxxx.luna.akamaiapis.net/config-gtm/v1/domains/"+gtmTestDomain+"/resources/"+GtmTestResource)
-        mock.
-                Delete("/config-gtm/v1/domains/"+gtmTestDomain+"/resources/"+GtmTestResource).
-                HeaderPresent("Authorization").
-                Reply(200).
-                SetHeader("Content-Type", "application/vnd.config-gtm.v1.3+json;charset=UTF-8").
-                BodyString(`{
+	mock := gock.New("https://akaa-baseurl-xxxxxxxxxxx-xxxxxxxxxxxxx.luna.akamaiapis.net/config-gtm/v1/domains/" + gtmTestDomain + "/resources/" + GtmTestResource)
+	mock.
+		Delete("/config-gtm/v1/domains/"+gtmTestDomain+"/resources/"+GtmTestResource).
+		HeaderPresent("Authorization").
+		Reply(200).
+		SetHeader("Content-Type", "application/vnd.config-gtm.v1.3+json;charset=UTF-8").
+		BodyString(`{
                         "resource" : null,
                         "status" : {
                                 "message" : "Change Pending",
@@ -318,12 +315,12 @@ func TestDeleteResource(t *testing.T) {
                         }
                }`)
 
-       Init(config)
+	Init(config)
 
-       getResource := instantiateResource()
-       stat, err := getResource.Delete(gtmTestDomain)
-       assert.NoError(t, err)
+	getResource := instantiateResource()
+	stat, err := getResource.Delete(gtmTestDomain)
+	assert.NoError(t, err)
 
-       assert.Equal(t, "9a7a8f84-704b-40de-a903-bcc2728513ac",stat.ChangeId) 
+	assert.Equal(t, "9a7a8f84-704b-40de-a903-bcc2728513ac", stat.ChangeId)
 
 }
