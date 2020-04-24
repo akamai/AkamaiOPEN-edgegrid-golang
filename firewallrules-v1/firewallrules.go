80de373cdfd2ec726dc7c0997eb4fef0348f9f04
Ian Cass <icass@akamai.com>
2020-04-24 16:04:20 +0100

    Added firewall rules

---
+++ firewallrules-v1/firewallrules.go
@@ -0,0 +1,178 @@
+package firewallrules
+
+import (
+	"encoding/json"
+	"bytes"
+        client "github.com/akamai/AkamaiOPEN-edgegrid-golang/client-v1"
+)
+
+type ListServicesResponse []struct {
+	ServiceID   int    `json:"serviceId"`
+	ServiceName string `json:"serviceName"`
+	Description string `json:"description"`
+}
+
+type Subscriptions struct {
+	ServiceID   int    `json:"serviceId"`
+	ServiceName string `json:"serviceName"`
+	Email       string `json:"email"`
+	SignupDate  string `json:"signupDate"`
+} 
+
+type ListSubscriptionsResponse struct {
+	Subscriptions []Subscriptions `json:"subscriptions"`
+}
+	
+type ListCidrBlocksResponse []struct {
+	CidrID        int    `json:"cidrId"`
+	ServiceID     int    `json:"serviceId"`
+	ServiceName   string `json:"serviceName"`
+	Cidr          string `json:"cidr"`
+	CidrMask      string `json:"cidrMask"`
+	Port          string `json:"port"`
+	CreationDate  string `json:"creationDate"`
+	EffectiveDate string `json:"effectiveDate"`
+	ChangeDate    string `json:"changeDate"`
+	MinIP         string `json:"minIp"`
+	MaxIP         string `json:"maxIp"`
+	LastAction    string `json:"lastAction"`
+}
+
+type UpdateSubscriptionsRequest struct {
+	Subscriptions []Subscriptions `json:"subscriptions"`
+}
+
+type UpdateSubscriptionsResponse struct {
+	Subscriptions []Subscriptions `json:"subscriptions"`
+}
+
+func ListSubscriptions() (*ListSubscriptionsResponse, error) {
+
+        req, err := client.NewRequest(
+                Config,
+                "GET",
+                "/firewall-rules-manager/v1/subscriptions",
+                nil,
+        )
+
+        if err != nil {
+                return nil, err
+        }
+
+        res, err := client.Do(Config, req)
+
+        if err != nil {
+                return nil, err
+        }
+
+        if client.IsError(res) {
+                return nil, client.NewAPIError(res)
+        }
+
+        var response ListSubscriptionsResponse
+        if err = client.BodyJSON(res, &response); err != nil {
+                return nil, err
+        }
+
+        return &response, nil
+
+}
+func ListServices() (*ListServicesResponse, error) {
+
+        req, err := client.NewRequest(
+                Config,
+                "GET",
+                "/firewall-rules-manager/v1/services",
+                nil,
+        )
+
+        if err != nil {
+                return nil, err
+        }
+
+        res, err := client.Do(Config, req)
+
+        if err != nil {
+                return nil, err
+        }
+
+        if client.IsError(res) {
+                return nil, client.NewAPIError(res)
+        }
+
+        var response ListServicesResponse
+        if err = client.BodyJSON(res, &response); err != nil {
+                return nil, err
+        }
+
+        return &response, nil
+
+}
+
+func UpdateSubscriptions(updatesubscriptionsrequest UpdateSubscriptionsRequest) (*UpdateSubscriptionsResponse, error) {
+
+        s, err := json.Marshal(updatesubscriptionsrequest)
+	if err != nil {
+		return nil, err
+	}
+
+        req, err := client.NewRequest(
+                Config,
+                "PUT",
+                "/firewall-rules-manager/v1/subscriptions",
+                bytes.NewReader(s),
+        )
+
+        if err != nil {
+                return nil, err
+        }
+
+        res, err := client.Do(Config, req)
+
+        if err != nil {
+                return nil, err
+        }
+
+        if client.IsError(res) {
+                return nil, client.NewAPIError(res)
+        }
+
+        var response UpdateSubscriptionsResponse
+        if err = client.BodyJSON(res, &response); err != nil {
+                return nil, err
+        }
+
+        return &response, nil
+
+}
+func ListCidrBlocks() (*ListCidrBlocksResponse, error) {
+
+        req, err := client.NewRequest(
+                Config,
+                "GET",
+                "/firewall-rules-manager/v1/cidr-blocks",
+                nil,
+        )
+
+        if err != nil {
+                return nil, err
+        }
+
+        res, err := client.Do(Config, req)
+
+        if err != nil {
+                return nil, err
+        }
+
+        if client.IsError(res) {
+                return nil, client.NewAPIError(res)
+        }
+
+        var response ListCidrBlocksResponse
+        if err = client.BodyJSON(res, &response); err != nil {
+                return nil, err
+        }
+
+        return &response, nil
+
+}
