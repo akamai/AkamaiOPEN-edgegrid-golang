99fafe046a78ed3ffc62d243d9fab439b917306a
López López, Roberto <rlopezlo@akamai.com>
2020-09-17 11:43:18 +0200

    TFP-170-v2-hostnames nok tests for get and update hostnames

--- pkg/papi/propertyhostname_test.go
+++ pkg/papi/propertyhostname_test.go
@@ -2,9 +2,12 @@ package papi
 
 import (
 	"context"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/akamai/AkamaiOPEN-edgegrid-golang/v2/pkg/session"
 
 	"github.com/stretchr/testify/require"
 	"github.com/tj/assert"
@@ -78,6 +81,50 @@ func TestPapi_GetHostnames(t *testing.T) {
 						},
 					},
 				},
+			},
+		},
+		"validation error PropertyID missing": {
+			params: GetHostnamesRequest{
+				PropertyVersion: 3,
+			},
+			withError: func(t *testing.T, err error) {
+				want := ErrStructValidation
+				assert.True(t, errors.Is(err, want), "want: %s; got: %s", want, err)
+				assert.Contains(t, err.Error(), "PropertyID")
+			},
+		},
+		"validation error PropertyVersion missing": {
+			params: GetHostnamesRequest{
+				PropertyID: "prp_175780",
+			},
+			withError: func(t *testing.T, err error) {
+				want := ErrStructValidation
+				assert.True(t, errors.Is(err, want), "want: %s; got: %s", want, err)
+				assert.Contains(t, err.Error(), "PropertyVersion")
+			},
+		},
+		"500 internal server status error": {
+			params: GetHostnamesRequest{
+				PropertyID:      "prp_175780",
+				PropertyVersion: 3,
+			},
+			responseStatus: http.StatusInternalServerError,
+			responseBody: `
+{
+	"type": "internal_error",
+    "title": "Internal Server Error",
+    "detail": "Error fetching hostnames",
+    "status": 500
+}`,
+			expectedPath: "/papi/v1/properties/prp_175780/versions/3/hostnames?contractId=&groupId=&validateHostnames=false",
+			withError: func(t *testing.T, err error) {
+				want := session.APIError{
+					Type:       "internal_error",
+					Title:      "Internal Server Error",
+					Detail:     "Error fetching hostnames",
+					StatusCode: http.StatusInternalServerError,
+				}
+				assert.True(t, errors.Is(err, want), "want: %s; got: %s", want, err)
 			},
 		},
 	}
@@ -171,6 +218,122 @@ func TestPapi_CreateHostnames(t *testing.T) {
 						},
 					},
 				},
+			},
+		},
+		"200 empty hostnames": {
+			params: CreateHostnamesRequest{
+				PropertyID:      "prp_175780",
+				PropertyVersion: 3,
+				GroupID:         "grp_15225",
+				ContractID:      "ctr_1-1TJZH5",
+			},
+			responseStatus: http.StatusOK,
+			responseBody: `
+{
+    "accountId": "act_1-1TJZFB",
+    "contractId": "ctr_1-1TJZH5",
+    "groupId": "grp_15225",
+    "propertyId": "prp_175780",
+    "propertyVersion": 3,
+    "etag": "6aed418629b4e5c0",
+    "hostnames": {
+        "items": []
+    }
+}
+
+`,
+			expectedPath: "/papi/v1/properties/prp_175780/versions/3/hostnames?contractId=ctr_1-1TJZH5&groupId=grp_15225&validateHostnames=false",
+			expectedResponse: &CreateHostnamesResponse{
+				AccountID:       "act_1-1TJZFB",
+				ContractID:      "ctr_1-1TJZH5",
+				GroupID:         "grp_15225",
+				PropertyID:      "prp_175780",
+				PropertyVersion: 3,
+				Etag:            "6aed418629b4e5c0",
+				Hostnames: HostnameItems{
+					Items: []HostnameItem{},
+				},
+			},
+		},
+		"200 VerifyHostnames true empty hostnames": {
+			params: CreateHostnamesRequest{
+				PropertyID:        "prp_175780",
+				PropertyVersion:   3,
+				GroupID:           "grp_15225",
+				ContractID:        "ctr_1-1TJZH5",
+				ValidateHostnames: true,
+			},
+			responseStatus: http.StatusOK,
+			responseBody: `
+{
+    "accountId": "act_1-1TJZFB",
+    "contractId": "ctr_1-1TJZH5",
+    "groupId": "grp_15225",
+    "propertyId": "prp_175780",
+    "propertyVersion": 3,
+    "etag": "6aed418629b4e5c0",
+	"validateHostnames": true,
+    "hostnames": {
+        "items": []
+    }
+}
+
+`,
+			expectedPath: "/papi/v1/properties/prp_175780/versions/3/hostnames?contractId=ctr_1-1TJZH5&groupId=grp_15225&validateHostnames=true",
+			expectedResponse: &CreateHostnamesResponse{
+				AccountID:       "act_1-1TJZFB",
+				ContractID:      "ctr_1-1TJZH5",
+				GroupID:         "grp_15225",
+				PropertyID:      "prp_175780",
+				PropertyVersion: 3,
+				Etag:            "6aed418629b4e5c0",
+				Hostnames: HostnameItems{
+					Items: []HostnameItem{},
+				},
+			},
+		},
+		"validation error PropertyID missing": {
+			params: CreateHostnamesRequest{
+				PropertyVersion: 3,
+			},
+			withError: func(t *testing.T, err error) {
+				want := ErrStructValidation
+				assert.True(t, errors.Is(err, want), "want: %s; got: %s", want, err)
+				assert.Contains(t, err.Error(), "PropertyID")
+			},
+		},
+		"validation error PropertyVersion missing": {
+			params: CreateHostnamesRequest{
+				PropertyID: "prp_175780",
+			},
+			withError: func(t *testing.T, err error) {
+				want := ErrStructValidation
+				assert.True(t, errors.Is(err, want), "want: %s; got: %s", want, err)
+				assert.Contains(t, err.Error(), "PropertyVersion")
+			},
+		},
+		"500 internal server status error": {
+			params: CreateHostnamesRequest{
+				PropertyID:      "prp_175780",
+				PropertyVersion: 3,
+			},
+			responseStatus: http.StatusInternalServerError,
+			responseBody: `
+{
+	"type": "internal_error",
+    "title": "Internal Server Error",
+    "detail": "Error updating hostnames",
+    "status": 500
+}`,
+			expectedPath: "/papi/v1/properties/prp_175780/versions/3/hostnames?contractId=&groupId=&validateHostnames=false",
+			withError: func(t *testing.T, err error) {
+				want := session.APIError{
+					Type:       "internal_error",
+					Title:      "Internal Server Error",
+					Detail:     "Error updating hostnames",
+					StatusCode: http.StatusInternalServerError,
+				}
+				assert.True(t, errors.Is(err, want), "want: %s; got: %s", want, err)
 			},
 		},
 	}
