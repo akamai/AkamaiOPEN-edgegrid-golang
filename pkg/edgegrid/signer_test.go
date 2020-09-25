09c80e3efbcf93e593a593730246fc4d4ceef44e
Rob Rodriguez <rrodrigu@akamai.com>
2020-09-25 06:58:59 -0700

    making the ResponseLink parsing less ambiguous

--- pkg/edgegrid/signer_test.go
+++ pkg/edgegrid/signer_test.go
@@ -12,24 +12,6 @@ import (
 	"github.com/tj/assert"
 )
 
-type Interface interface {
-	WithFields(Fielder) *Entry
-	WithField(string, interface{}) *Entry
-	WithDuration(time.Duration) *Entry
-	WithError(error) *Entry
-	Debug(string)
-	Info(string)
-	Warn(string)
-	Error(string)
-	Fatal(string)
-	Debugf(string, ...interface{})
-	Infof(string, ...interface{})
-	Warnf(string, ...interface{})
-	Errorf(string, ...interface{})
-	Fatalf(string, ...interface{})
-	Trace(string) *Entry
-}
-
 func TestConfig_createAuthHeader(t *testing.T) {
 	tests := map[string]struct {
 		config    Config
