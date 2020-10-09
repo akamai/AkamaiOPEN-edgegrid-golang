da6dff6382b29406cf7218901fe5c14df654be87
Rob Rodriguez <rrodrigu@akamai.com>
2020-10-09 06:17:37 -0700

    domain_test complete

--- pkg/configgtm/domain_test.go
+++ pkg/configgtm/domain_test.go
@@ -352,18 +352,29 @@ func TestGtm_CreateDomain(t *testing.T) {
 	}
 }
 
-/*
 // Test Update domain.
 // UpdateDomain(context.Context, *Domain, map[string]string) (*DomainResponse, error)
-func TestDns_UpdateDomain(t *testing.T) {
+func TestGtm_UpdateDomain(t *testing.T) {
+	var result DomainResponse
+
+	respData, err := loadTestData("TestGtm_UpdateDomain.resp.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := json.NewDecoder(bytes.NewBuffer(respData)).Decode(&result); err != nil {
+		t.Fatal(err)
+	}
+
 	tests := map[string]struct {
-		domain         Domain
-		query          map[string]string
-		responseStatus int
-		responseBody   string
-		expectedPath   string
-		withError      error
-		headers        http.Header
+		domain           Domain
+		query            map[string]string
+		responseStatus   int
+		responseBody     []byte
+		expectedPath     string
+		expectedResponse *ResponseStatus
+		withError        error
+		headers          http.Header
 	}{
 		"200 Success": {
 			domain: Domain{
@@ -371,190 +382,28 @@ func TestDns_UpdateDomain(t *testing.T) {
 				Name:                  "gtmdomtest.akadns.net",
 				Type:                  "basic",
 			},
-			query: map[string]string{contractId: "1-2ABCDE"},
+			query: map[string]string{"contractId": "1-2ABCDE"},
 			headers: http.Header{
 				"Content-Type": []string{"application/vnd.config-gtm.v1.4+json;charset=UTF-8"},
 			},
-			responseStatus: http.StatusCreated,
-			responseBody: `
-                        {
-                          "resource" : {
-                                "cnameCoalescingEnabled" : false,
-                                "defaultErrorPenalty" : 75,
-                                "defaultHealthMax" : null,
-                                "defaultHealthMultiplier" : null,
-                                "defaultHealthThreshold" : null,
-                                "defaultMaxUnreachablePenalty" : null,
-                                "defaultSslClientCertificate" : null,
-                                "defaultSslClientPrivateKey" : null,
-                                "defaultTimeoutPenalty" : 25,
-                                "defaultUnreachableThreshold" : null,
-                                "emailNotificationList" : [ ],
-                                "endUserMappingEnabled" : false,
-                                "lastModified" : "2019-06-24T18:48:57.787+00:00",
-                                "lastModifiedBy" : "operator",
-                                "loadFeedback" : false,
-                                "mapUpdateInterval" : 0,
-                                "maxProperties" : 0,
-                                "maxResources" : 512,
-                                "maxTestTimeout" : 0.0,
-                                "maxTTL" : 0,
-                                "minPingableRegionFraction" : null,
-                                "minTestInterval" : 0,
-                                "minTTL" : 0,
-                                "modificationComments" : null,
-                                "name" : "gtmdomtest.akadns.net",
-                                "pingInterval" : null,
-                                "pingPacketSize" : null,
-                                "roundRobinPrefix" : null,
-                                "servermonitorLivenessCount" : null,
-                                "servermonitorLoadCount" : null,
-                                "servermonitorPool" : null,
-                                "type" : "basic",
-                                "status" : {
-                                        "message" : "Change Pending",
-                                        "changeId" : "539872cc-6ba6-4429-acd5-90bab7fb5e9d",
-                                        "propagationStatus" : "PENDING",
-                                        "propagationStatusDate" : "2019-06-24T18:48:57.787+00:00",
-                                        "passingValidation" : true,
-                                        "links" : [ {
-                                                "rel" : "self",
-                                                "href" : "https://akaa-32qkzqewderdchot-d3uwbyqc4pqi2c5l.luna-dev.akamaiapis.net/config-gtm/v1/domains/gtmdomtest.akadns.net/status/current"
-                                        } ]
-                                },
-                                "loadImbalancePercentage" : null,
-                                "domainVersionId" : null,
-                                "resources" : [ ],
-                                "properties" : [ ],
-                                "datacenters" : [ ],
-                                "geographicMaps" : [ ],
-                                "cidrMaps" : [ ],
-                                "asMaps" : [ ],
-                                "links" : [ {
-                                        "rel" : "self",
-                                        "href" : "https://akaa-32qkzqewderdchot-d3uwbyqc4pqi2c5l.luna-dev.akamaiapis.net/config-gtm/v1/domains/gtmdomtest.akadns.net"
-                                    }, {
-                                        "rel" : "datacenters",
-                                        "href" : "https://akaa-32qkzqewderdchot-d3uwbyqc4pqi2c5l.luna-dev.akamaiapis.net/config-gtm/v1/domains/gtmdomtest.akadns.net/datacenters"
-                                    }, {
-                                        "rel" : "properties",
-                                        "href" : "https://akaa-32qkzqewderdchot-d3uwbyqc4pqi2c5l.luna-dev.akamaiapis.net/config-gtm/v1/domains/gtmdomtest.akadns.net/properties"
-                                    }, {
-                                        "rel" : "geographic-maps",
-                                        "href" : "https://akaa-32qkzqewderdchot-d3uwbyqc4pqi2c5l.luna-dev.akamaiapis.net/config-gtm/v1/domains/gtmdomtest.akadns.net/geographic-maps"
-                                    }, {
-                                        "rel" : "cidr-maps",
-                                        "href" : "https://akaa-32qkzqewderdchot-d3uwbyqc4pqi2c5l.luna-dev.akamaiapis.net/config-gtm/v1/domains/gtmdomtest.akadns.net/cidr-maps"
-                                    }, {
-                                        "rel" : "resources",
-                                        "href" : "https://akaa-32qkzqewderdchot-d3uwbyqc4pqi2c5l.luna-dev.akamaiapis.net/config-gtm/v1/domains/gtmdomtest.akadns.net/resources"
-                                    }, {
-                                        "rel" : "as-maps",
-                                        "href" : "https://akaa-32qkzqewderdchot-d3uwbyqc4pqi2c5l.luna-dev.akamaiapis.net/config-gtm/v1/domains/gtmdomtest.akadns.net/as-maps"
-                                    } ]
-                                },
-                        "status" : {
-                                "message" : "Change Pending",
-                                "changeId" : "539872cc-6ba6-4429-acd5-90bab7fb5e9d",
-                                "propagationStatus" : "PENDING",
-                                "propagationStatusDate" : "2019-06-24T18:48:57.787+00:00",
-                                "passingValidation" : true,
-                                "links" : [ {
-                                        "rel" : "self",
-                                        "href" : "https://akaa-32qkzqewderdchot-d3uwbyqc4pqi2c5l.luna-dev.akamaiapis.net/config-gtm/v1/domains/gtmdomtest.akadns.net/status/current"
-                                } ]
-                        }`,
-			expectedResponse: &DomainResponse{
-				Resource: {
-					CnameCoalescingEnabled: false,
-					DefaultErrorPenalty:    75,
-					DefaultTimeoutPenalty:  25,
-					EndUserMappingEnabled:  false,
-					LastModified:           "2019-06-24T18:48:57.787+00:00",
-					LastModifiedBy:         "operator",
-					LoadFeedback:           false,
-					MapUpdateInterval:      0,
-					MaxProperties:          0,
-					MaxResources:           512,
-					MaxTestTimeout:         0.0,
-					MaxTTL:                 0,
-					MinTestInterval:        0,
-					MinTTL:                 0,
-					Name:                   "gtmdomtest.akadns.net",
-					Type:                   "basic",
-					Status: *ResponseStatus{
-						Message:               "Change Pending",
-						ChangeId:              "539872cc-6ba6-4429-acd5-90bab7fb5e9d",
-						PropagationStatus:     "PENDING",
-						PropagationStatusDate: "2019-06-24T18:48:57.787+00:00",
-						PassingValidation:     true,
-						Links: []*Link{
-							{
-								Rel:  "self",
-								Href: "https://akaa-32qkzqewderdchot-d3uwbyqc4pqi2c5l.luna-dev.akamaiapis.net/config-gtm/v1/domains/gtmdomtest.akadns.net/status/current",
-							},
-						},
-					},
-					Links: []*Link{
-						{
-							Rel:  "self",
-							Href: "https://akaa-32qkzqewderdchot-d3uwbyqc4pqi2c5l.luna-dev.akamaiapis.net/config-gtm/v1/domains/gtmdomtest.akadns.net",
-						},
-						{
-							Rel:  "datacenters",
-							Href: "https://akaa-32qkzqewderdchot-d3uwbyqc4pqi2c5l.luna-dev.akamaiapis.net/config-gtm/v1/domains/gtmdomtest.akadns.net/datacenters",
-						},
-						{
-							Rel:  "properties",
-							Href: "https://akaa-32qkzqewderdchot-d3uwbyqc4pqi2c5l.luna-dev.akamaiapis.net/config-gtm/v1/domains/gtmdomtest.akadns.net/properties",
-						},
-						{
-							Rel:  "geographic-maps",
-							Href: "https://akaa-32qkzqewderdchot-d3uwbyqc4pqi2c5l.luna-dev.akamaiapis.net/config-gtm/v1/domains/gtmdomtest.akadns.net/geographic-maps",
-						},
-						{
-							Rel:  "cidr-maps",
-							Href: "https://akaa-32qkzqewderdchot-d3uwbyqc4pqi2c5l.luna-dev.akamaiapis.net/config-gtm/v1/domains/gtmdomtest.akadns.net/cidr-maps",
-						},
-						{
-							Rel:  "resources",
-							Href: "https://akaa-32qkzqewderdchot-d3uwbyqc4pqi2c5l.luna-dev.akamaiapis.net/config-gtm/v1/domains/gtmdomtest.akadns.net/resources",
-						},
-						{
-							Rel:  "as-maps",
-							Href: "https://akaa-32qkzqewderdchot-d3uwbyqc4pqi2c5l.luna-dev.akamaiapis.net/config-gtm/v1/domains/gtmdomtest.akadns.net/as-maps",
-						},
-					},
-				},
-				Status: &ResponseStatus{
-					Message:           "Change Pending",
-					ChangeId:          "40e36abd-bfb2-4635-9fca-62175cf17007",
-					PropagationStatus: "PENDING",
-					PassingValidation: true,
-					Links: []*Link{
-						{
-							Href: "https://akaa-32qkzqewderdchot-d3uwbyqc4pqi2c5l.luna-dev.akamaiapis.net/config-gtm/v1/domains/gtmdomtest.akadns.net/status/current",
-							Rel:  "self",
-						},
-					},
-				},
-			},
-			expectedPath: "/config-gtm/v1/domains?contractId=1-2ABCDE",
+			responseStatus:   http.StatusCreated,
+			responseBody:     respData,
+			expectedResponse: result.Status,
+			expectedPath:     "/config-gtm/v1/domains?contractId=1-2ABCDE",
 		},
 		"500 internal server error": {
 			domain: Domain{
 				Name: "gtmdomtest.akadns.net",
 				Type: "basic",
 			},
-			query:          map[string]string{contractId: "1-2ABCDE"},
+			query:          map[string]string{"contractId": "1-2ABCDE"},
 			responseStatus: http.StatusInternalServerError,
-			responseBody: `
+			responseBody: []byte(`
 {
     "type": "internal_error",
     "title": "Internal Server Error",
-    "detail": "Error creating zone",
-    "status": 500
-}`,
+    "detail": "Error creating zone"
+}`),
 			expectedPath: "/config-gtm/v1/domains?contractId=1-2ABCDE",
 			withError: &Error{
 				Type:       "internal_error",
@@ -571,12 +420,12 @@ func TestDns_UpdateDomain(t *testing.T) {
 				assert.Equal(t, http.MethodPut, r.Method)
 				w.WriteHeader(test.responseStatus)
 				if len(test.responseBody) > 0 {
-					_, err := w.Write([]byte(test.responseBody))
+					_, err := w.Write(test.responseBody)
 					assert.NoError(t, err)
 				}
 			}))
 			client := mockAPIClient(t, mockServer)
-			err := client.UpdateDomain(
+			result, err := client.UpdateDomain(
 				session.ContextWithOptions(
 					context.Background(),
 					session.WithContextHeaders(test.headers)), &test.domain, test.query)
@@ -585,44 +434,7 @@ func TestDns_UpdateDomain(t *testing.T) {
 				return
 			}
 			require.NoError(t, err)
+			assert.Equal(t, test.expectedResponse, result)
 		})
 	}
 }
-
-/* Future. Presently no domain Delete endpoint.
-func TestDeleteDomain(t *testing.T) {
-
-        defer gock.Off()
-
-        mock := gock.New("https://akaa-baseurl-xxxxxxxxxxx-xxxxxxxxxxxxx.luna.akamaiapis.net/config-gtm/v1/domains/"+gtmTestDomain)
-        mock.
-                Delete("/config-gtm/v1/domains/"+gtmTestDomain).
-                HeaderPresent("Authorization").
-                Reply(200).
-                SetHeader("Content-Type", "application/vnd.config-gtm.v1.4+json;charset=UTF-8").
-                BodyString(`{
-                        "resource" : null,
-                        "status" : {
-                               "changeId": "40e36abd-bfb2-4635-9fca-62175cf17007",
-                               "links": [
-                                     {
-                                          "href": "https://akab-ymtebc45gco3ypzj-apz4yxpek55y7fyv.luna.akamaiapis.net/config-gtm/v1/domains/gtmdomtest.akadns.net/status/current",
-                                          "rel": "self"
-                                     }
-                               ],
-                               "message": "Change Pending",
-                               "passingValidation": true,
-                               "propagationStatus": "PENDING",
-                               "propagationStatusDate": "2019-04-25T14:54:00.000+00:00"
-                          },
-                }`)
-
-        Init(config)
-
-        getDomain := instantiateDomain()
-
-        _, err := getDomain.Delete()
-        assert.NoError(t, err)
-
-}
-*/
