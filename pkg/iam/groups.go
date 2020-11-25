89b273f1d85f0ea0ae00a482fdcca6c4c73f9825
Rob Rodriguez <rrodrigu@akamai.com>
2020-11-25 07:18:39 -0800

    adding ListGroups

--- pkg/iam/groups.go
+++ pkg/iam/groups.go
@@ -1,6 +1,14 @@
 package iam
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/url"
+	"path"
+
+	"github.com/spf13/cast"
+)
 
 type (
 	// Groups is the IAM group management interface
@@ -33,6 +41,33 @@ type (
 	}
 )
 
-func (i *iam) ListGroups(context.Context, ListGroupsRequest) ([]Group, error) {
-	return nil, nil
+func (i *iam) ListGroups(ctx context.Context, params ListGroupsRequest) ([]Group, error) {
+	logger := i.Log(ctx)
+	logger.Debug("ListRoles")
+
+	u, err := url.Parse(path.Join(UserAdminEP, "groups"))
+	if err != nil {
+		return nil, fmt.Errorf("%s: failed to create request: %s", "ListGroups", err)
+	}
+	q := u.Query()
+	q.Add("actions", cast.ToString(params.Actions))
+
+	u.RawQuery = q.Encode()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, u.String(), nil)
+	if err != nil {
+		return nil, fmt.Errorf("%s: failed to create request: %s", "ListGroups", err)
+	}
+
+	var rval []Group
+	resp, err := i.Exec(req, &rval)
+	if err != nil {
+		return nil, fmt.Errorf("%s: request failed: %s", "ListGroups", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("%s: %w", "ListGroups", i.Error(resp))
+	}
+
+	return rval, nil
 }
