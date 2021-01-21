3aa12539a8d69107c52d09c4709528f9fb7f48e3
martinstibbe <33664051+martinstibbe@users.noreply.github.com>
2021-01-20 20:15:33 -0600

    Add support for removal of allow sampling and override

--- pkg/appsec/advanced_settings_logging.go
+++ pkg/appsec/advanced_settings_logging.go
@@ -24,6 +24,7 @@ type (
 	AdvancedSettingsLogging interface {
 		GetAdvancedSettingsLogging(ctx context.Context, params GetAdvancedSettingsLoggingRequest) (*GetAdvancedSettingsLoggingResponse, error)
 		UpdateAdvancedSettingsLogging(ctx context.Context, params UpdateAdvancedSettingsLoggingRequest) (*UpdateAdvancedSettingsLoggingResponse, error)
+		RemoveAdvancedSettingsLogging(ctx context.Context, params RemoveAdvancedSettingsLoggingRequest) (*RemoveAdvancedSettingsLoggingResponse, error)
 	}
 
 	GetAdvancedSettingsLoggingRequest struct {
@@ -89,7 +90,19 @@ type (
 	}
 
 	RemoveAdvancedSettingsLoggingResponse struct {
-		Action string `json:"action"`
+		Override      bool `json:"override"`
+		AllowSampling bool `json:"allowSampling"`
+		Cookies       struct {
+			Type string `json:"type"`
+		} `json:"cookies"`
+		CustomHeaders struct {
+			Type   string   `json:"type"`
+			Values []string `json:"values"`
+		} `json:"customHeaders"`
+		StandardHeaders struct {
+			Type   string   `json:"type"`
+			Values []string `json:"values"`
+		} `json:"standardHeaders"`
 	}
 )
 
@@ -103,6 +116,14 @@ func (v GetAdvancedSettingsLoggingRequest) Validate() error {
 
 // Validate validates UpdateAdvancedSettingsLoggingRequest
 func (v UpdateAdvancedSettingsLoggingRequest) Validate() error {
+	return validation.Errors{
+		"ConfigID": validation.Validate(v.ConfigID, validation.Required),
+		"Version":  validation.Validate(v.Version, validation.Required),
+	}.Filter()
+}
+
+// Validate validates UpdateAdvancedSettingsLoggingRequest
+func (v RemoveAdvancedSettingsLoggingRequest) Validate() error {
 	return validation.Errors{
 		"ConfigID": validation.Validate(v.ConfigID, validation.Required),
 		"Version":  validation.Validate(v.Version, validation.Required),
@@ -187,6 +208,53 @@ func (p *appsec) UpdateAdvancedSettingsLogging(ctx context.Context, params Updat
 	req.Header.Set("Content-Type", "application/json")
 	var rval UpdateAdvancedSettingsLoggingResponse
 	resp, err := p.Exec(req, &rval, params.JsonPayloadRaw)
+	if err != nil {
+		return nil, fmt.Errorf("create AdvancedSettingsLogging request failed: %w", err)
+	}
+
+	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusCreated {
+		return nil, p.Error(resp)
+	}
+
+	return &rval, nil
+}
+
+// Remove will update a AdvancedSettingsLogging.
+//
+// API Docs: // appsec v1
+//
+// https://developer.akamai.com/api/cloud_security/application_security/v1.html#putadvancedsettingslogging
+
+func (p *appsec) RemoveAdvancedSettingsLogging(ctx context.Context, params RemoveAdvancedSettingsLoggingRequest) (*RemoveAdvancedSettingsLoggingResponse, error) {
+	if err := params.Validate(); err != nil {
+		return nil, fmt.Errorf("%w: %s", ErrStructValidation, err.Error())
+	}
+
+	logger := p.Log(ctx)
+	logger.Debug("RemoveAdvancedSettingsLogging")
+
+	var putURL string
+	if params.PolicyID != "" {
+		putURL = fmt.Sprintf(
+			"/appsec/v1/configs/%d/versions/%d/security-policies/%s/advanced-settings/logging",
+			params.ConfigID,
+			params.Version,
+			params.PolicyID)
+	} else {
+		putURL = fmt.Sprintf(
+			"/appsec/v1/configs/%d/versions/%d/advanced-settings/logging",
+			params.ConfigID,
+			params.Version)
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPut, putURL, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create create AdvancedSettingsLoggingrequest: %w", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	var rval RemoveAdvancedSettingsLoggingResponse
+	resp, err := p.Exec(req, &rval, params)
 	if err != nil {
 		return nil, fmt.Errorf("create AdvancedSettingsLogging request failed: %w", err)
 	}
