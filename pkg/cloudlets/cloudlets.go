39eb64bc0ee5ab340f92411ff83cb78f704ba3fe
Michal Wojcik <miwojci@akamai.com>
2021-08-06 12:56:31 +0200

    TFP-856 Cloudlets - Edgegrid goland - initial structure

---
+++ pkg/cloudlets/cloudlets.go
@@ -0,0 +1,40 @@
+package cloudlets
+
+import (
+	"errors"
+
+	"github.com/akamai/AkamaiOPEN-edgegrid-golang/v2/pkg/session"
+)
+
+var (
+	// ErrStructValidation is returned when given struct validation failed
+	ErrStructValidation = errors.New("struct validation")
+)
+
+type (
+	// Cloudlets is the api interface for cloudlets
+	Cloudlets interface {
+	}
+
+	cloudlets struct {
+		session.Session
+	}
+
+	// Option defines a Cloudlets option
+	Option func(*cloudlets)
+
+	// ClientFunc is a Cloudlets client new method, this can be used for mocking
+	ClientFunc func(sess session.Session, opts ...Option) Cloudlets
+)
+
+// Client returns a new cloudlets Client instance with the specified controller
+func Client(sess session.Session, opts ...Option) Cloudlets {
+	c := &cloudlets{
+		Session: sess,
+	}
+
+	for _, opt := range opts {
+		opt(c)
+	}
+	return c
+}
