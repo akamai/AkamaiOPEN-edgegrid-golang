0980b3acff1704f45b30b2d370187bffd7b1fc3d
Piotr Piotrowski <pipiotro@akamai.com>
2021-12-02 12:12:37 +0100

    DXE-54 Edgeworkers API initial structure

---
+++ pkg/edgeworkers/edgeworkers.go
@@ -0,0 +1,39 @@
+package edgeworkers
+
+import (
+	"errors"
+
+	"github.com/akamai/AkamaiOPEN-edgegrid-golang/v2/pkg/session"
+)
+
+var (
+	// ErrStructValidation is returned when given struct validation failed
+	ErrStructValidation = errors.New("struct validation")
+)
+
+type (
+	// Edgeworkers is the api interface for edgeworkers
+	Edgeworkers interface{}
+
+	edgeworkers struct {
+		session.Session
+	}
+
+	// Option defines an Edgeworkers option
+	Option func(*edgeworkers)
+
+	// ClientFunc is a Edgeworkers client new method, this can be used for mocking
+	ClientFunc func(sess session.Session, opts ...Option) Edgeworkers
+)
+
+// Client returns a new edgeworkers Client instance with the specified controller
+func Client(sess session.Session, opts ...Option) Edgeworkers {
+	c := &edgeworkers{
+		Session: sess,
+	}
+
+	for _, opt := range opts {
+		opt(c)
+	}
+	return c
+}
---
+++ pkg/edgeworkers/errors_test.go
@@ -0,0 +1,101 @@
+package edgeworkers
+
+import (
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/akamai/AkamaiOPEN-edgegrid-golang/v2/pkg/session"
+	"github.com/stretchr/testify/require"
+	"github.com/tj/assert"
+)
+
+func TestNewError(t *testing.T) {
+	sess, err := session.New()
+	require.NoError(t, err)
+
+	req, err := http.NewRequest(
+		http.MethodHead,
+		"/",
+		nil)
+	require.NoError(t, err)
+
+	tests := map[string]struct {
+		response *http.Response
+		expected *Error
+	}{
+		"valid response, status code 500": {
+			response: &http.Response{
+				Status:     "Internal Server Error",
+				StatusCode: http.StatusInternalServerError,
+				Body: ioutil.NopCloser(strings.NewReader(
+					`{"type":"a","title":"b","detail":"c","status":500}`),
+				),
+				Request: req,
+			},
+			expected: &Error{
+				Type:   "a",
+				Title:  "b",
+				Detail: "c",
+				Status: http.StatusInternalServerError,
+			},
+		},
+		"invalid response body, assign status code": {
+			response: &http.Response{
+				Status:     "Internal Server Error",
+				StatusCode: http.StatusInternalServerError,
+				Body: ioutil.NopCloser(strings.NewReader(
+					`test`),
+				),
+				Request: req,
+			},
+			expected: &Error{
+				Title:  "test",
+				Detail: "",
+				Status: http.StatusInternalServerError,
+			},
+		},
+	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			res := Client(sess).(*edgeworkers).Error(test.response)
+			assert.Equal(t, test.expected, res)
+		})
+	}
+}
+
+func TestIs(t *testing.T) {
+	tests := map[string]struct {
+		err      Error
+		target   Error
+		expected bool
+	}{
+		"different error code": {
+			err:      Error{Status: 404},
+			target:   Error{Status: 401},
+			expected: false,
+		},
+		"same error code": {
+			err:      Error{Status: 404},
+			target:   Error{Status: 404},
+			expected: true,
+		},
+		"same error code and title": {
+			err:      Error{Status: 404, Title: "some error"},
+			target:   Error{Status: 404, Title: "some error"},
+			expected: true,
+		},
+		"same error code and different error message": {
+			err:      Error{Status: 404, Title: "some error"},
+			target:   Error{Status: 404, Title: "other error"},
+			expected: false,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, test.err.Is(&test.target), test.expected)
+		})
+	}
+}
